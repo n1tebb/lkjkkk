a7a6dcbcedfefbd9af7ae20998d6b76b80c922ef
n1tebb <kolasnikovegor@gmai.com>
2024-09-19 14:25:42 +0500

    калькулятор

--- test/main.cpp
+++ test/main.cpp
@@ -1,9 +1,50 @@
 #include <iostream>
 
 using namespace std;
-int main () {
-    
-    cout << "hello world";
-    
+
+int main() {
+    int a, b, c;
+    char ope;
+
+    cout << "введи число: " << endl;
+    cin >> a;
+    cout << "введи второе число: : " << endl;
+    cin >> b;
+
+    cout << "введи операцию (+, -, *, /, %): ";
+    cin >> ope;
+
+    if (ope == '+') {
+        c = a + b;
+        cout << "результат: " << c << endl;
+    }
+    else if (ope == '-') {
+        c = a - b;
+        cout << "результат: " << c << endl;
+    }
+    else if (ope == '*') {
+        c = a * b;
+        cout << "результат: " << c << endl;
+    }
+    else if (ope == '/') {
+        if (b != 0) {
+            c = a / b;
+            cout << "результат: " << c << endl;
+        } else {
+            cout << "ошибка: Деление на ноль!" << endl;
+        }
+    }
+    else if (ope == '%') {
+        if (b != 0) {
+            c = a % b;
+            cout << "результат: " << c << endl;
+        } else {
+            cout << "Ошибка: Деление на ноль!" << endl;
+        }
+    }
+    else {
+        cout << "ввыбрана не верная операция" << endl;
+    }
+
     return 0;
 }
