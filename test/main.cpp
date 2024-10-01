992f3e4a4e9d8129866b0414236e0fedd1a60e1f
n1tebb <kolasnikovegor@gmai.com>
2024-10-01 13:38:02 +0500

    mmm

--- test/main.cpp
+++ test/main.cpp
@@ -1,47 +1,27 @@
 #include <iostream>
+#include <cstdlib>
 
-using namespace std;
 
 int main() {
-    double firstNumber, secondNumber, result;
-    char operation;
-
-    cout << "Введите первое число: ";
-    cin >> firstNumber;
-
-    cout << "Введите операцию (+, -, *, /, %): ";
-    cin >> operation;
-
-    cout << "Введите второе число: ";
-    cin >> secondNumber;
-
-    if (operation == '+') {
-        result = firstNumber + secondNumber;
-        cout << "Результат: " << firstNumber << " + " << secondNumber << " = " << result << endl;
+    srand(time(NULL));
+    int a = 0;
+    a = rand() % 6 + 5;
+    
+    const int size = 20;
+    int arr[size];
+    
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = a;
     }
-    else if (operation == '-') {
-        result = firstNumber - secondNumber;
-        cout << "Результат: " << firstNumber << " - " << secondNumber << " = " << result << endl;
-    }
-    else if (operation == '*') {
-        result = firstNumber * secondNumber;
-        cout << "Результат: " << firstNumber << " * " << secondNumber << " = " << result << endl;
-    }
-    else if (operation == '/') {
-        if (secondNumber != 0) {
-            result = firstNumber / secondNumber;
-            cout << "Результат: " << firstNumber << " / " << secondNumber << " = " << result << endl;
-        } else {
-            cout << "Ошибка: Деление на ноль запрещено!" << endl;
+    
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << arr[i];
+        if ((i + 1) % 10 == 0)
+        {
+            std::cout << std::endl;
         }
     }
-    else if (operation == '%') {
-        result = (firstNumber / 100) * secondNumber;
-        cout << "Резульат: " << secondNumber << "% от " << firstNumber << " = " << result << endl;
-    }
-    else {
-        cout << "Ошибка! Неверная операция!" << endl;
-    }
-
     return 0;
 }
