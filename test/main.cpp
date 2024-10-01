eb2b88c7da86d007f75baff43556a33072580ddd
n1tebb <kolasnikovegor@gmai.com>
2024-10-01 15:05:13 +0500

    hjvbhh

--- test/main.cpp
+++ test/main.cpp
@@ -3,21 +3,39 @@
 
 
 int main() {
-    const int size = 20;
-    int arr[size];
+    srand(time(NULL));
+    const int size = 10;
+    int randomarr[size];
+    int userarr[size];
     
-    for (int i = 0; i < size; i++)
-    {
-        arr[i] = std::rand() % 6 + 5;
+    for (int i = 0; i < size; i++) {
+        randomarr[i] = std::rand() % 11;
     }
     
-    for (int i = 0; i < size; i++)
-    {
-        std::cout << arr[i];
-        if ((i + 1) % 10 == 0)
-        {
-            std::cout << std::endl;
+    std::cout << "Введите 10 чисел от 0 до 10: ";
+    for (int i = 0; i < size; i++) {
+        int num;
+        do {
+            std::cout << "число для ячейки " << i + 1 << ": ";
+            std::cin >> num;
+            if (num < 0 || num > 10) {
+                std::cout << "Ошибка: чило не корректнро.\n";
+            }
+        } while (num < 0 || num > 10);
+        userarr[i] = num;
+        
+        std::cout << "\nРезультат сравнения: ";
+        bool hits = false;
+        for (int i = 0; i < size; i++) {
+            if (userarr[i] == randomarr[i]) {
+                std::cout << "попадание в ячейке " << i + 1 << ": число " << userarr[i] << "\n";
+                hits = true;
+            }
         }
+        if (!hits) {
+            std::cout << "попаданий нету.\n";
+        }
+        
     }
     return 0;
 }
